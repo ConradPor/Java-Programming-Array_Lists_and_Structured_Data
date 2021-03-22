e45222cbe53d8d3d23b7d8333e09b5edf878656d
ConradPor <konradporoszewski@yahoo.com>
2021-03-22 23:18:58 +0100

    Programming Exercise: Breaking the Caesar Cipher Full code Caesar Breaker.

---
+++ 1. Week/CaesarBreaker/src/CaesarBreaker.java
@@ -0,0 +1,103 @@
+import edu.duke.FileResource;
+
+public class CaesarBreaker {
+
+
+
+    //This method count letters in the String
+    public static int[] countLetters(String message) {
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        int[] counts = new int[26];
+        for (int i = 0; i < message.length(); i++) {
+            char ch = Character.toLowerCase(message.charAt(i));
+            int inx = alphabet.indexOf(ch);
+            if (inx != -1) {
+                counts[inx] += 1;
+            }
+        }
+        return counts;
+    }
+
+
+    //This method calculate the index of the largest letter frequency
+    public static int maxIndex(int[] vals) {
+        int max = 0;
+        for (int i = 0; i < vals.length; i++) {
+            if (vals[i] > vals[max]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+
+    /*The method decryptTwoKeys has one parameter, a String parameter named encrypted that represents
+    a String that was encrypted with the two key. This method attempts to determine the two keys used
+    to encrypt the message, prints the two keys, and then returns the decrypted
+    String with those two keys.*/
+
+    public String decryptTwoKeys(String encrypted) {
+        CaesarCipher cc = new CaesarCipher();
+        int dkey1 = getKey(halfOfString(encrypted, 0));
+        int dkey2 = getKey(halfOfString(encrypted, 1));
+        System.out.println(dkey1 + "\t" + dkey2);
+        String message = cc.encryptTwoKeys(encrypted, 26 - dkey1, 26 - dkey2);
+        return message;
+    }
+
+    public static String decrypt(String encrypted, int key) {
+
+        CaesarCipher cc = new CaesarCipher();
+        String message = cc.encrypt(encrypted, 26-key);
+        return message;
+    }
+
+    /*The method halfOfString that has two parameters, a String parameter named message
+    and an int parameter named start. This method should return a new String that is every other character
+    from message starting with the start position.*/
+
+    public String halfOfString(String message, int start) {
+        StringBuilder half = new StringBuilder();
+        for (int i = start; i < message.length(); i += 2) {
+            char a = message.charAt(i);
+            half.append(a);
+        }
+        return half.toString();
+    }
+
+
+    /*Method get key call countLetters to get an array of the letter frequencies in String s and then use
+    maxIndex to calculate the index of the largest letter frequency, which is the location of the encrypted letter ‘e’,
+    which leads to the key, which is returned.*/
+
+    public int getKey(String s) {
+        int[] freqs = countLetters(s);
+        int maxIn = maxIndex(freqs);
+        int dkey = maxIn - 4;
+        if (maxIn < 4) {
+            dkey = 26 - (4 - maxIn);
+        }
+        return dkey;
+    }
+
+    public void testDecrypt (){
+        FileResource fr = new FileResource();
+        String message = fr.asString();
+        int dke = getKey(message);
+        String dec = decrypt(message, dke);
+        System.out.println("Key is: "+ dke+ "\nDekrypted massage: "+ dec);
+    }
+
+    public void testDecryptTwoKeys () {
+        FileResource fr = new FileResource();
+        String message = fr.asString();
+        String dec = decryptTwoKeys(message);
+        System.out.println("Dekrypted massage: "+ dec);
+
+    }
+
+    /*public static void main(String[] args) {
+        CaesarBreaker cc = new CaesarBreaker();
+        //cc.testDecrypt();
+        cc.testDecryptTwoKeys();
+    }*/
+}
---
+++ 1. Week/CaesarBreaker/src/CaesarCipher.java
@@ -0,0 +1,120 @@
+import edu.duke.FileResource;
+
+public class CaesarCipher {
+
+    //Method return encrypted string using the Caesar Cipher algorithm.
+    public String encrypt (String input, int key) {
+        StringBuilder encrypted = new StringBuilder (input);
+        String Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String alphabet = Alphabet.toLowerCase();
+        String ShiftedAlphabet = Alphabet.substring(key)+ Alphabet.substring(0,key);
+        String shiftedAlphabet = alphabet.substring(key)+ alphabet.substring(0,key);
+        for (int i = 0; i < encrypted.length();i++){
+            char currChar = encrypted.charAt(i);
+            if (Character.isLowerCase(currChar)) {
+                int inx = alphabet.indexOf(currChar);
+                if (inx !=-1){
+                    char newChar = shiftedAlphabet.charAt(inx);
+                    encrypted.setCharAt(i,newChar);
+                }
+            }
+            else {
+                int inx = Alphabet.indexOf(currChar);
+                if (inx !=-1){
+                    char newChar = ShiftedAlphabet.charAt(inx);
+                    encrypted.setCharAt(i,newChar);
+                }
+            }
+        }
+        return encrypted.toString();
+    }
+
+
+    /* Method encryptTwoKeys returns a String that has been encrypted using the following algorithm.
+        Parameter key1 is used to encrypt every second character with the Caesar Cipher algorithm,
+        starting with the first character, and key2 is used to encrypt every second character,
+        starting with the second character.*/
+
+    public String encryptTwoKeys(String input, int key1, int key2) {
+        StringBuilder encrypted = new StringBuilder (input);
+        String Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String alphabet = Alphabet.toLowerCase();
+        String ShiftedAlphabet1 = Alphabet.substring(key1)+ Alphabet.substring(0,key1);
+        String shiftedAlphabet1 = alphabet.substring(key1)+ alphabet.substring(0,key1);
+        String ShiftedAlphabet2 = Alphabet.substring(key2)+ Alphabet.substring(0,key2);
+        String shiftedAlphabet2 = alphabet.substring(key2)+ alphabet.substring(0,key2);
+
+        for (int i = 0; i <encrypted.length();i+=2){
+            char currChar = encrypted.charAt(i);
+            if ((i %2 == 0) && (Character.isLowerCase(currChar))) {
+                int inx = alphabet.indexOf(currChar);
+                if (inx!= 0)
+                {
+                    char newChar = shiftedAlphabet1.charAt(inx);
+                    encrypted.setCharAt(i,newChar);
+                }
+            }
+
+            else if ((i %2 == 0) && (Character.isUpperCase(currChar)))
+            {
+                int idx = Alphabet.indexOf(currChar);
+                if (idx != 0)
+                {
+                    char newChar = ShiftedAlphabet1.charAt(idx);
+                    encrypted.setCharAt(i,newChar);
+                }
+            }
+
+        }
+
+        for (int i = 1; i < encrypted.length(); i+=2){
+            char currChar = encrypted.charAt(i);
+            if ((i %2 != 0) && (Character.isLowerCase(currChar)))
+            {
+                int inx = alphabet.indexOf(currChar);
+                if (inx != 0)
+                {
+                    char newChar = shiftedAlphabet2.charAt(inx);
+                    encrypted.setCharAt(i, newChar);
+                }
+            }
+            else if ((i %2 != 0) && (Character.isUpperCase(currChar))) {
+                int inx = Alphabet.indexOf(currChar);
+                if (inx != 0)
+                {
+                    char newChar = ShiftedAlphabet2.charAt(inx);
+                    encrypted.setCharAt(i,newChar);
+                }
+            }
+
+        }
+
+        return encrypted.toString();
+    }
+
+    // Test method to check encrypt method
+    public void testCaesar() {
+        int key = 23;
+        FileResource fr = new FileResource();
+        String message = fr.asString();
+        String encrypted = encrypt(message, key);
+        System.out.println("Key is: "+ key+"\n"+encrypted);
+    }
+
+    // Method to check encryptTwoKeys method
+    public void testEncryptTwoKeys() {
+        int key1 = 10;
+        int key2 = 7;
+        FileResource fr = new FileResource();
+        String message = fr.asString();
+        String encrypted = encryptTwoKeys(message, key1, key2);
+        System.out.println("Key1 is: "+ key1 + " and Key2 is: "+ key2 +"\n"+encrypted);
+
+    }
+
+    public static void main(String[] args) {
+        CaesarCipher cc = new CaesarCipher();
+        cc.testCaesar();
+        //cc.testEncryptTwoKeys();
+    }
+}
